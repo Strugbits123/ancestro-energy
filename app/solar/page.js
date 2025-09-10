4d9b01f7a5cc0cadaac928bfe30a084b673d3d2b
Alina Soomro <alina.soomro@yahoo.com>
2025-09-11 01:17:00 +0500

    hover card

--- app/solar/page.js
+++ app/solar/page.js
@@ -1,58 +1,99 @@
+import FlagHoverModal from "@/src/components/FlagModal";
 import SunButton from "@/src/components/SunButton";
 
+const caseStudy = [
+    {
+        country: 'COLOMBIA', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'MEXICO', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'CHILE', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'COSTA-RICA', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'COLOMBIA', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'MEXICO', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'DOMINICAN', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'ECUADOR', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'GUATEMALA', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'HONDURAS', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'NICARAGUA', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'PANAMA', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'PARAGUAY', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'PERU', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'SALVADOR', category: 'FACTORY/INDUSTRIAL PLANT', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+    {
+        country: 'URUGUAY', category: 'WAREHOUSE/LOGISTICS CENTRE', investmentData: ['TIME TO COMPLETE PROJECT:150 DAYS', 'IMMEDIATE SAVINGS: 12% O&M COST', 'LONG TERM SAVINGS: $150,000+'], description: "Our factory's energy bills were suffocating us. With Ancestro, we started saving from day one—no upfront costs. In just two months, the system was running, and we now project hundreds of thousands in lifetime savings. It has given us financial stability, room to expand production, and the confidence to plan long-term.\n— Carlos M., Factory Owner, Colombia"
+    },
+]
+
 export default function Solar() {
+
+
     return (
         <section className="relative min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-800 via-gray-900 to-black">
             {/* Background */}
             <div className="absolute inset-0">
                 <div
-                    className="w-full h-full"
+                    className="w-full h-full bg-black/50"
                     style={{
                         backgroundImage: `url('/images/solar-hero-section.png')`,
                         backgroundRepeat: 'no-repeat',
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
-                        backgroundColor: 'rgba(0,0,0,0.1)',
+                        backgroundColor: 'rgba(0,0,0,0.5)',
                     }}
+                // className="w-full h-full bg-black/50 bg-[url('/images/solar-hero-section.png')] bg-no-repeat bg-cover bg-center"
+
                 />
             </div>
 
             <div className="container-2xl mx-auto px-[55px] text-center relative z-10 py-20">
                 <div className="flex flex-col xl:flex-row gap-10 items-stretch p-20">
                     {/* Left Side: Heading + Flags */}
-                    <div className="flex-3 flex flex-col items-start justify-center gap-4 w-[45%]">
-                        <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-lato font-bold text-white leading-tight text-start tracking-wide uppercase max-w-6xl mx-auto">
+                    <div className="flex-3 flex flex-col items-start justify-center gap-4 w-full xl:w-[45%]">
+                        <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-lato font-bold text-white leading-tight text-start tracking-wide uppercase max-w-6xl mx-auto">
                             Join LATAM’s Fastest-Growing Solar Subscription Network.
                         </h1>
                         <span className="text-2xl text-left text-white font-lato">$0 upfront. Day-one savings. Long-term stability. Available now in 14 countries.</span>
                         <div className="flex flex-wrap justify-center items-center gap-8">
-                            {[
-                                "chile",
-                                "colombia",
-                                "costa-rica",
-                                "dominican",
-                                "ecuador",
-                                "guatemala",
-                                "honduras",
-                                "mexico",
-                                "nicaragua",
-                                "panama",
-                                "paraguay",
-                                "peru",
-                                "salvador",
-                                "uruguay",
-                            ].map((country, i) => (
-                                <img
+                            {caseStudy.map((study, i) => (
+                                <FlagHoverModal
                                     key={i}
-                                    src={`/images/${country}.png`}
-                                    alt={`${country} flag`}
-                                    className="w-28 h-16 rounded-sm object-cover shadow"
+                                    content={{
+                                        src: `/images/${study.country.toLowerCase()}.png`,
+                                        alt: `${study.country}`,
+                                        study: study
+                                    }}
                                 />
                             ))}
                         </div>
-
-                        <div className="flex justify-start w-full xl:w-[35%]"><SunButton className="border-yellow-300 text-yellow-200">Apply Now — Join the Solar Tribe</SunButton></div>
-
+                        <div className="flex justify-start w-full xl:w-[35%]"><SunButton className="border-yellow-300 text-base text-yellow-200 bg-gradient-to-b from-yellow-100/20 via-yellow-300/20 to-yellow-500/20">Apply Now — Join the Solar Tribe</SunButton></div>
                     </div>
 
                     {/* Right Side: Image */}
---
+++ src/components/FlagModal.js
@@ -0,0 +1,68 @@
+"use client"
+import { useState } from "react";
+
+const FlagHoverModal = ({ content }) => {
+    const [show, setShow] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseEnter = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        setPosition({
+            x: rect.right + 20,
+            y: rect.top - 200,
+        });
+        setShow(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShow(false);
+    };
+
+    return (
+        <div
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="relative inline-block"
+        >
+            <img
+                src={content.src}
+                alt={content.alt}
+                className="w-28 h-16 rounded-sm object-cover shadow"
+            />
+
+            {show && (
+                <div
+                    className="flex flex-col items-center justify-center absolute z-50  shadow-lg h-fit max-w-[280px]  rounded p-4 text-sm w-fit bg-gradient-to-r from-white/10 via-white/20 to-white/30 border border-white/30 backdrop-blur-lg rounded-[20px]"
+                    style={{
+                        top: position.y,
+                        left: position.x,
+                        position: "fixed",
+                    }}
+                >
+                    <img className="h-[45%] w-auto" src={'/images/solar-panel-case-study.png'} />
+
+                    <div className="mt-2 flex w-full justify-start items-center gap-2">
+                        <h1 className="text-white text-2xl font-bold">{`${content.alt.toUpperCase()}`} </h1>
+                        <p className="text-white mt-2">{`${content.study.category}`} </p>
+                    </div>
+                    <div className="flex flex-wrap gap-[5px]">
+                        {content.study.investmentData.map((data, i) => (
+                            <div key={i} className="text-[8px] text-white p-2 bg-gradient-to-r from-white/10 via-white/20 to-white/30 border border-white/30 backdrop-blur-lg rounded-full">
+                                {data}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="px-1 pb-2 pt-1 mt-2 text-white text-[9px] text-start">
+                        {content.study.description}
+                    </div>
+                    <div className="flex justify-between w-full mb-3">
+                        <div className="text-white text-md">VIDEO TESTIMONIAL<strong className="ml-2 text-xl">→</strong></div>
+                        <img className='w-8 h-8' src={'/images/customer-card.png'} />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default FlagHoverModal;
